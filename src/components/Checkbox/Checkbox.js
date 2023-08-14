df91b12e4b68bf2355db7c585d05088abfd0b144
Mykhailo Stryzhka <misha.strizhcko@gmail.com>
2023-08-14 23:13:18 +0200

    Add contact method

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -4,10 +4,17 @@ import IconCheckboxChack from 'images/icons/IconCheckboxChack';
 
 const Checkbox = e => {
   return (
-    <StyledLabel>
+    <StyledLabel
+      onChange={() => e.form.setFieldValue(e.field.name, e.field.value)}
+    >
       <StyledField type={e.type} name={e.field.name} />
-      {!e.field.value && <IconCheckbox />}
-      {e.field.value && <IconCheckboxChack />}
+      {e.type === 'radio'
+        ? (e.field.value !== e.form.values[e.field.name] && <IconCheckbox />) ||
+          (e.field.value === e.form.values[e.field.name] && (
+            <IconCheckboxChack />
+          ))
+        : (!e.field.value && <IconCheckbox />) ||
+          (e.field.value && <IconCheckboxChack />)}
     </StyledLabel>
   );
 };
