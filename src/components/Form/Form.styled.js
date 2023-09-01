a0b1bd4fcebb136afb1b64a4fa4b8362cd9df063
Mykhailo Stryzhka <misha.strizhcko@gmail.com>
2023-09-01 23:34:54 +0200

    Add universall form

---
+++ src/components/Form/Form.styled.js
@@ -0,0 +1,34 @@
+import { Form } from 'formik';
+import { styled } from 'styled-components';
+
+export const StyledForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  flex-grow: 3;
+  padding: 40px 0 75px 0;
+`;
+
+export const FormDescription = styled.p`
+  width: 800px;
+
+  color: ${({ theme }) => theme.color.placeholder};
+
+  font-family: ${({ theme }) => theme.fontFamily};
+  font-size: 16px;
+  font-style: italic;
+  font-weight: 400;
+  line-height: normal;
+
+  > span {
+    color: red;
+  }
+`;
+
+export const BtnBox = styled.div`
+  padding-top: 80px;
+
+  display: flex;
+  justify-content: space-between;
+`;
