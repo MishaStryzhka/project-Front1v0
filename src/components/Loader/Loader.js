66b3651f66dd562ef3dea769ef75498ddf38f386
Mykhailo Stryzhka <misha.strizhcko@gmail.com>
2023-09-01 23:35:15 +0200

    Update Loader.js
    Adds info loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,11 +1,24 @@
 import { LoaderWraper, Spinner } from './Loader.styled';
 
-const Loader = ({ width, height, urlImage, colorParent, className }) => {
+// ============================
+
+// import Loader from 'components/Loader/Loader';
+// import Image from 'images/Loader.png';
+
+// <Loader height={300} width={300} $urlImage={Image} />
+// <Loader height={100} width={100} $urlImage={Image} />
+// <Loader height={20} width={20} $urlImage={Image} />
+
+// ============================
+
+const Loader = ({ width, height, $urlImage, colorParent, className }) => {
+  console.log('urlImage', $urlImage);
+
   return (
     <LoaderWraper
       width={width}
       height={height}
-      $urlImage={urlImage}
+      $urlImage={$urlImage}
       className={className}
     >
       <Spinner
