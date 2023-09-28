3ec27b0e8feddd74af802e3cf5bfb1d7c29c007a
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-09-29 00:28:24 +0200

    update

--- src/componentsReusable/Calendar/Calendar.styled.js
+++ src/componentsReusable/Calendar/Calendar.styled.js
@@ -5,17 +5,17 @@ padding:10px;
 display:flex;
 justify-content:center;
 align-items:center;
-/* min-height:480px; */
 .react-calendar__navigation {
     display: flex;
     padding: 10px 4px;
-    margin-bottom:14px;
+    /* margin-bottom:14px; */
     }
     .react-calendar__navigation__label {
         font-family: "Rubik";
         font-size: 16px;
         text-align: center;
         font-weight: 400;
+        text-transform: capitalize;
     }
 
     .react-calendar__navigation__arrow {
@@ -25,6 +25,7 @@ align-items:center;
     .react-calendar__month-view__weekdays {
     text-align: center;
     text-decoration:none;
+    border:none;
     margin-bottom: 14px;
     line-height:1.5;
     font-family: "Rubik";
@@ -32,27 +33,17 @@ align-items:center;
     font-weight: 400;
     color:#00185C;
    }
+
+   .react-calendar__month-view__weekdays__weekday{
+    text-transform: capitalize;
+    color:rgba(0, 24, 92, 1);
+   }
   
 
-.react-calendar__month-view__weekdays::first-letter {
-    text-transform: uppercase;
-  }
-
   .button {
-    /* margin: 3px; */
     background-color: transparent;
     border: transparent;
     border-radius: 3px;
-    color: white;
-    padding: 5px 0;
-
-    &:hover {
-      background-color: #556b55;
-    }
-
-    &:active {
-      background-color: #a5c1a5;
-    }
   }
 
   .react-calendar__month-view__days{
@@ -61,9 +52,47 @@ align-items:center;
     font-weight: 400;
     display: grid !important;
     grid-template-columns: 40px 40px 40px 40px 40px 40px 40px;
-    grid-template-rows: 40px 40px 40px 40px 40px 40px 40px;
+    grid-template-rows: 40px 40px 40px 40px 40px 40px;
     color:#00185C !important;
    }
    .react-calendar__tile {
       max-width: initial !important;
-    }`
+    }
+    
+    .react-calendar__month-view__days__day--neighboringMonth{
+        color:#BABABA !important;
+    }
+    
+    .react-calendar__month-view__days__day{
+        color:rgba(0, 24, 92, 1);
+    }
+    
+.react-calendar__tile--active{
+background-color: rgba(57, 109, 255, 1);
+color:white;
+border-radius:12px;
+}
+
+.react-calendar__navigation__label__labelText--from{
+    display:flex;
+    justify-content:space-between;
+}
+
+.react-calendar__year-view__months{
+    display:flex;
+    flex-direction:column;
+}
+.react-calendar__year-view__months, 
+  .react-calendar__decade-view__years, 
+  .react-calendar__century-view__decades {
+    display: grid !important;
+    grid-template-columns: 100%;
+    grid-template-rows:  18% 18% 18% 18% 18% 18% 18% 18% 18% 18% 18% 18% ;
+  }
+
+  .react-calendar__year-view__months__month{
+    text-transform: capitalize;
+    color:rgba(0, 24, 92, 1);
+    font-family: "Rubik";
+    font-size: 16px;
+  }`
