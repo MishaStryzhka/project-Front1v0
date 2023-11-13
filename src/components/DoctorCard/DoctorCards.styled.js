b943344573b16cabe6a90cfeb07e79de77b2bad7
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-11-14 00:16:09 +0100

    add corrects

--- src/components/DoctorCard/DoctorCards.styled.js
+++ src/components/DoctorCard/DoctorCards.styled.js
@@ -13,25 +13,28 @@ export const Item = styled.li`
   align-items: center;
   position: relative;
   gap: 80px;
-  background-color: rgba(255, 255, 255, 1);
+
+  background-color: ${({ theme }) => theme.color.primary};
 `;
 export const TitleDescription = styled.h3`
+  font-family: ${({ theme }) => theme.fontFamily};
   font-weight: 500;
   line-height: normal;
   font-size: 20px;
-  font-family: 'Rubik', sans-serif;
-  color: rgba(57, 109, 255, 1);
   position: absolute;
   right: 660px;
+
+  color: ${({ theme }) => theme.color.main};
 `;
 
 export const Description = styled.p`
-  font-family: 'Rubik', sans-serif;
+  font-family: ${({ theme }) => theme.fontFamily};
   font-size: 20px;
   font-weight: 400;
   line-height: normal;
   margin-left: 320px;
-  color: rgba(0, 24, 92, 1);
+
+  color: ${({ theme }) => theme.color.text};
 `;
 
 export const Wrap = styled.div`
